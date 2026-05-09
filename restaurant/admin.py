1af38d982d2a12fb1ca31f345fc3ca6ad241a4b1
Mariia Bidenko <mbidenk@gmail.com>
2026-05-09 17:46:26 +0200

    Admin setup: creating Django Administration, establishing Username, Email and Password. Adding two models to the Django Administration: Universities and User

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import University, User
+from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
+@admin.register(University)
+class UniversityAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email_domain', 'is_active')
+    search_fields = ('name', 'email_domain')
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'role', 'is_verified_student', 'university')
+    list_filter = ('role', 'is_verified_student', 'university')
+    fieldsets = UserAdmin.fieldsets + (
+        ('Extra info', {'fields': ('role', 'is_verified_student', 'university')}),
+    )
