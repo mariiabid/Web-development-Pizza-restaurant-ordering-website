8dc6f96eeecf4c8e799a10e81e93862d7df41890
Mariia Bidenko <mbidenk@gmail.com>
2026-05-09 17:16:00 +0200

    Model creation: Class University and class User

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class University(models.Model):
+    name = models.CharField(max_length=200, unique = True)
+    email_domain = models.CharField(max_length=100, unique = True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+    
+class User(AbstractUser):
+    ROLE_CHOICES = (
+        ('customer', 'Customer'),
+        ('staff', 'Staff'),
+    )
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default = 'customer')
+    is_verified_student = models.BooleanField(default=False)
+    university = models.ForeignKey(
+        University, 
+        on_delete=models.SET_NULL, 
+        null=True, blank = True
+    )
+    def __str__(self):
+        return self.username
