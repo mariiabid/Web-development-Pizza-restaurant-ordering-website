26ab3d83c2dea7f2f840e3b77360ea89348590e0
Daria <dbidenk@example.com>
2026-05-16 20:16:02 +0200

    URLs and Views Update/Creation: Add URLs and basic views, create Template folder for HTML files

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+
+def home(request):
+    return render(request, 'restaurant/home.html')
+
+def menu(request):
+    pizzas = Product.objects.filter(product_type = 'pizza', is_available = True)
+    drinks =  Product.objects.filter(product_type = 'drink', is_available = True)
+    context = {
+        'pizzas':pizzas,
+        'drinks':drinks,
+    }
+    return render(request, 'restaurant/menu.html')
